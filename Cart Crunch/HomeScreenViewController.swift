5070a541c91e53d64484715b1dcd25f983704ef1
Gabriel Jones <gabe1122k@gmail.com>
2023-04-20 11:57:56 -0500

    Removed buttons, and view controllers
    removed buttons on home and search and watchlist viewcontrollers

--- Cart Crunch/HomeScreenViewController.swift
+++ Cart Crunch/HomeScreenViewController.swift
@@ -18,21 +18,6 @@ class HomeScreenViewController: UIViewController {
         return table
     }()
     
-    let scrollView: UIScrollView = {
-        //needed for the horizontally scrollable buttons
-        let scrollView = UIScrollView()
-        scrollView.showsHorizontalScrollIndicator = false
-        return scrollView
-    }()
-    
-    let stackView: UIStackView = {
-        //needed for the horizontally scrollable buttons
-        let stackView = UIStackView()
-        stackView.axis = .horizontal
-        stackView.spacing = 30
-        return stackView
-    }()
-    
     let searchBar: UISearchBar = {
         let search = UISearchBar()
         search.placeholder = "Search"
@@ -44,73 +29,10 @@ class HomeScreenViewController: UIViewController {
         return search
     }()
     
-    let testButton1: UIButton = {
-        let button = UIButton()
-        button.setTitle("Brand", for: .normal)
-        button.titleLabel?.font = UIFont.systemFont(ofSize: 13)
-        button.setTitleColor(.black, for: .normal)
-        button.layer.borderWidth = 1
-        button.layer.cornerRadius = 5
-        button.backgroundColor = UIColor(red: 233/255, green: 233/255, blue: 233/255, alpha: 1.0)
-        return button
-    }()
-    
-    let testButton2: UIButton = {
-        let button = UIButton()
-        button.setTitle("Brand", for: .normal)
-        button.titleLabel?.font = UIFont.systemFont(ofSize: 13)
-        button.setTitleColor(.black, for: .normal)
-        button.layer.borderWidth = 1
-        button.layer.cornerRadius = 5
-        button.backgroundColor = UIColor(red: 233/255, green: 233/255, blue: 233/255, alpha: 1.0)
-        return button
-    }()
-    
-    let testButton3: UIButton = {
-        let button = UIButton()
-        button.setTitle("Brand", for: .normal)
-        button.titleLabel?.font = UIFont.systemFont(ofSize: 13)
-        button.setTitleColor(.black, for: .normal)
-        button.layer.borderWidth = 1
-        button.layer.cornerRadius = 5
-        button.backgroundColor = UIColor(red: 233/255, green: 233/255, blue: 233/255, alpha: 1.0)
-        return button
-    }()
-    
-    let testButton4: UIButton = {
-        let button = UIButton()
-        button.setTitle("Brand", for: .normal)
-        button.titleLabel?.font = UIFont.systemFont(ofSize: 13)
-        button.setTitleColor(.black, for: .normal)
-        button.layer.borderWidth = 1
-        button.layer.cornerRadius = 5
-        button.backgroundColor = UIColor(red: 233/255, green: 233/255, blue: 233/255, alpha: 1.0)
-        return button
-    }()
-    
-    let testButton5: UIButton = {
-        let button = UIButton()
-        button.setTitle("Brand", for: .normal)
-        button.titleLabel?.font = UIFont.systemFont(ofSize: 13)
-        button.setTitleColor(.black, for: .normal)
-        button.layer.borderWidth = 1
-        button.layer.cornerRadius = 5
-        button.backgroundColor = UIColor(red: 233/255, green: 233/255, blue: 233/255, alpha: 1.0)
-        return button
-    }()
-    
-    func setupButtonTargets() {
-        testButton1.addTarget(self, action: #selector(onButtonTapped), for: .touchUpInside)
-        testButton2.addTarget(self, action: #selector(onButtonTapped), for: .touchUpInside)
-        testButton3.addTarget(self, action: #selector(onButtonTapped), for: .touchUpInside)
-        testButton4.addTarget(self, action: #selector(onButtonTapped), for: .touchUpInside)
-        testButton5.addTarget(self, action: #selector(onButtonTapped), for: .touchUpInside)
-    }
-
     //MARK: - Network manager
     //so we can access the methods from this class
     let networkManager = NetworkManager()
-
+    
     //MARK: - Product object array
     var displayedProducts: [Product] = []
     
@@ -118,7 +40,6 @@ class HomeScreenViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         setupUI()
-        setupButtonTargets()
         tableView.delegate = self
         tableView.dataSource = self
         view.backgroundColor = .white
@@ -134,28 +55,18 @@ class HomeScreenViewController: UIViewController {
                     self.tableView.reloadData()
                     print(self.displayedProducts)
                 }
-
+                
             case .failure(let error):
                 print(error)
             }
         }
     }
-    
     //MARK: - setupUI function
     private func setupUI() {
         view.addSubview(searchBar)
-        view.addSubview(scrollView)
-        scrollView.addSubview(stackView)
-        stackView.addArrangedSubview(testButton1)
-        stackView.addArrangedSubview(testButton2)
-        stackView.addArrangedSubview(testButton3)
-        stackView.addArrangedSubview(testButton4)
-        stackView.addArrangedSubview(testButton5)
         view.addSubview(tableView)
         
         searchBar.translatesAutoresizingMaskIntoConstraints = false
-        scrollView.translatesAutoresizingMaskIntoConstraints = false
-        stackView.translatesAutoresizingMaskIntoConstraints = false
         tableView.translatesAutoresizingMaskIntoConstraints = false
         
         NSLayoutConstraint.activate([
@@ -163,54 +74,11 @@ class HomeScreenViewController: UIViewController {
             searchBar.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -10),
             searchBar.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 10),
             
-            scrollView.topAnchor.constraint(equalTo: searchBar.bottomAnchor, constant: 15),
-            scrollView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
-            scrollView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
-            scrollView.heightAnchor.constraint(equalToConstant: 30),
-
-            stackView.topAnchor.constraint(equalTo: scrollView.topAnchor),
-            stackView.leadingAnchor.constraint(equalTo: scrollView.leadingAnchor),
-            stackView.trailingAnchor.constraint(equalTo: scrollView.trailingAnchor),
-            stackView.bottomAnchor.constraint(equalTo: scrollView.bottomAnchor),
-            stackView.heightAnchor.constraint(equalTo: scrollView.heightAnchor),
-            
-            testButton1.widthAnchor.constraint(equalToConstant: 60),
-            testButton1.heightAnchor.constraint(equalToConstant: 30),
-            
-            testButton2.widthAnchor.constraint(equalToConstant: 60),
-            testButton2.heightAnchor.constraint(equalToConstant: 30),
-
-            testButton3.widthAnchor.constraint(equalToConstant: 60),
-            testButton3.heightAnchor.constraint(equalToConstant: 30),
-            
-            testButton4.widthAnchor.constraint(equalToConstant: 60),
-            testButton4.heightAnchor.constraint(equalToConstant: 30),
-            
-            testButton5.widthAnchor.constraint(equalToConstant: 60),
-            testButton5.heightAnchor.constraint(equalToConstant: 30),
-            
-            tableView.topAnchor.constraint(equalTo: testButton1.bottomAnchor, constant: 25),
+            tableView.topAnchor.constraint(equalTo: searchBar.bottomAnchor, constant: 25),
             tableView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
             tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
             tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor)
         ])
-    }
-    
-    @objc private func onButtonTapped(_ sender: UIButton) {
-        //adding toggle to the buttons for the color but this is also where we can let the user
-        //turn off the brand filters by clicking the already selected button
-        if sender.backgroundColor == UIColor(red: 249/255, green: 119/255, blue: 82/255, alpha: 1.0) {
-            sender.backgroundColor = UIColor(red: 233/255, green: 233/255, blue: 233/255, alpha: 1.0)
-        } else {
-            sender.backgroundColor = UIColor(red: 249/255, green: 119/255, blue: 82/255, alpha: 1.0)
-        }
-        
-        
-        //change all the other buttons to the gray color
-        let buttons = [testButton1, testButton2, testButton3, testButton4, testButton5]
-        for button in buttons where button != sender {
-            button.backgroundColor = UIColor(red: 233/255, green: 233/255, blue: 233/255, alpha: 1.0)
-        }
     }
 }
 
