01dd75029ae8caafcef7c064fe110befe217acc3
Jonathan Velez <110303080+Jav0918@users.noreply.github.com>
2023-04-24 15:35:35 -0500

    Store locator
    map was pinned to screen with test data for annotations. function works for placing annotations. need to connect api data

---
+++ Cart Crunch/SheetTableViewController.swift
@@ -0,0 +1,44 @@
+//
+//  SheetViewController.swift
+//  Cart Crunch
+//
+//  Created by Jonathan Velez on 4/20/23.
+//
+import Foundation
+import UIKit
+
+class SheetViewController: UITableViewController  {
+
+    let items = Range(0...20).map {_ in "($0)"}
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+
+        //view.backgroundColor = .white
+
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "ItemCell")
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+
+        return items.count
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ItemCell", for: indexPath)
+
+        var content = cell.defaultContentConfiguration()
+
+        content.text = "Item \(indexPath.row)"
+
+        cell.contentConfiguration = content
+
+        return cell
+    }
+
+
+
+}
